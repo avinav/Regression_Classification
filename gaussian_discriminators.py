7c62ff25cac7ad8234a91563826cd714f989359b
avinav <sharan.avinav@gmail.com>
2015-04-04 06:33:52 -0400

    LDA implementation and results

--- gaussian_discriminators.py
+++ gaussian_discriminators.py
@@ -7,6 +7,7 @@ import scipy.linalg as linalg
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import pickle
+from matplotlib.patches import Ellipse
 def ldaLearn(X,y):
     # Inputs
     # X - a N x d matrix with each row corresponding to a training example
@@ -17,8 +18,17 @@ def ldaLearn(X,y):
     # covmat - A single d x d learnt covariance matrix 
     
     # IMPLEMENT THIS METHOD
+    N = X.shape[0]
+    d = X.shape[1]
+    # initialization
+    y = y.reshape(y.size)
+    classes = np.unique(y)
+    means = np.zeros((d,classes.size))
     
-    return    
+    for cl in range(classes.size):
+        means[:,cl] = np.mean(X[y==classes[cl]],0) 
+    
+    return means, np.cov(X.T)   
     #return means,covmat
 
 def qdaLearn(X,y):
@@ -31,9 +41,19 @@ def qdaLearn(X,y):
     # covmats - A list of k d x d learnt covariance matrices for each of the k classes
     
     # IMPLEMENT THIS METHOD
+    N = X.shape[0]
+    d = X.shape[1]
+    # initialization
+    y = y.reshape(y.size)
+    classes = np.unique(y)
+    means = np.zeros((d,classes.size))
+    covmats = [np.zeros((d,d))] * classes.size
     
-    return
-    #return means,covmats
+    for cl in range(classes.size):
+        means[:,cl] = np.mean(X[y==classes[cl]],0)
+        covmats[cl] = np.cov(X[y==classes[cl]].T)
+
+    return means,covmats
 
 def ldaTest(means,covmat,Xtest,ytest):
     # Inputs
@@ -44,9 +64,17 @@ def ldaTest(means,covmat,Xtest,ytest):
     # acc - A scalar accuracy value
     
     # IMPLEMENT THIS METHOD
-    
-    return
-    #return acc
+    invcovmat = linalg.inv(covmat)
+    covmatdet = linalg.det(covmat)
+    ydist = np.zeros((Xtest.shape[0],means.shape[1]))
+    for i in range(means.shape[1]):
+        ydist[:,i] = np.exp(-0.5*np.sum((Xtest - means[:,i])* 
+        np.dot(invcovmat, (Xtest - means[:,i]).T).T,1)/(np.sqrt(np.pi*2)*(covmatdet**2)))
+    ylabel = np.argmax(ydist,1)
+    ylabel = ylabel + 1
+    ytest = ytest.reshape(ytest.size)
+    acc = 100*np.mean(ylabel == ytest)
+    return acc, ylabel
 
 def qdaTest(means,covmats,Xtest,ytest):
     # Inputs
@@ -57,47 +85,94 @@ def qdaTest(means,covmats,Xtest,ytest):
     # acc - A scalar accuracy value
     
     # IMPLEMENT THIS METHOD
-    
-    return
-    #return acc
-    
-#------------------Script Starts Here------------------------------
-# Load sample data
-data = pickle.load(open("sample.pickle","rb"))
-[data0, data1, data2, data3] = data
 
-# Plot sample data 
+    ydist = np.zeros((Xtest.shape[0],means.shape[1]))
+    for i in range(means.shape[1]):
+        invcovmat = linalg.inv(covmats[i])
+        covmatdet = linalg.det(covmat[i])
+        ydist[:,i] = np.exp(-0.5*np.sum((Xtest - means[:,i])* 
+        np.dot(invcovmat, (Xtest - means[:,i]).T).T,1)/(np.sqrt(np.pi*2)*(covmatdet**2)))
+    ylabel = np.argmax(ydist,1)
+    ylabel = ylabel + 1
+    ytest = ytest.reshape(ytest.size)
+    acc = 100*np.mean(ylabel == ytest)
+    return acc, ylabel    
+    #return acc
+
+def plot_data(data0, label0, title, *args):
+    
+    colors = iter(cm.rainbow(np.linspace(0,1,5)))
+    plt.clf()
+    fig = plt.figure()
+
+    for i in range(1,6):
+        plt.scatter(data0[(label0==i).reshape((label0==i).size),0],
+        data0[(label0==i).reshape((label0==i).size),1],color=next(colors))
+        # Draw ellipse with 1 standard deviation
+        if (len(args) == 2):
+            ax = plt.gca()
+            # Compute eigen vectors and value for covmat to plot an ellipse
+            means, covmats = args
+            lambda_, v = np.linalg.eig(covmats[i-1])
+            lambda_ = np.sqrt(lambda_)
+            ell = Ellipse(xy=(means[0,i-1], means[1,i-1]),
+                        width=lambda_[0]*2, height=lambda_[1]*2,
+                        angle=np.rad2deg(np.arccos(v[0, 0])))
+            ell.set_facecolor('none')
+            ax.add_artist(ell)
+            #Add mean points
+            plt.scatter(means[0,i-1], means[1,i-1],c='black',s=30);
+    fig.suptitle(title)
+    fig.show()
+    return
+            
+#------------------Script Starts Here------------------------------
+# Close figures
 plt.close("all")
 
-colors = iter(cm.rainbow(np.linspace(0,1,5)))
-plt.clf()
-fig = plt.figure()
-for i in range(1,6):
-    plt.scatter(data0[(data1==i).reshape((data1==i).size),0],data0[(data1==i).reshape((data1==i).size),1],color=next(colors))
-fig.suptitle('Sample data 1')
-fig.show()
+# Load sample data
+data = pickle.load(open("sample.pickle","rb"))
+[data0, label0, test0, label1] = data
 
-colors = iter(cm.rainbow(np.linspace(0,1,5)))
-fig = plt.figure()
-for i in range(1,6):
-    plt.scatter(data2[(data3==i).reshape((data3==i).size),0],data2[(data3==i).reshape((data3==i).size),1],color=next(colors))
-fig.suptitle('Sample data 2')
-fig.show()
+# LDA Learn means and covariance
+means, covmat = ldaLearn(data0,label0)
+#covmat[0,1] = 0
+#covmat[1,0] = 0
+acc, ylabel = ldaTest(means,covmat,test0,label1)
+print acc
+# Create meshgrid
+xx, yy = np.meshgrid(np.linspace(0,16,100), np.linspace(0,16,100))
+grid = np.vstack((xx.reshape(xx.size),yy.reshape(yy.size))).T
+acctemp, gridlabel = ldaTest(means,covmat, grid, label1)
+plot_data(grid,gridlabel,'LDA Accuracy: ' + str(acc) +'%', means,[covmat]*means.shape[1])
+
+# Plot sample data with LDA data
+#plot_data(data0,label0,'(LDA) Sample data 1 with class mean and common covariance(1 SD)',
+#means,[covmat]*means.shape[1])
+#plot_data(data1,label1,'Sample data 2')        
+
+# QDA Learn mean and covariances
+qda_means, qda_covmats = qdaLearn(data0, label0)
+
+# Plot sample data with QDA data
+#plot_data(data0,label0,'(QDA) Sample data 1 with class mean and covariances(1 SD)',
+#qda_means,qda_covmats)
 
 # Load diabetes data
 dtdata = pickle.load(open('diabetes.pickle','rb'))
-[dtdata0, dtdata1, dtdata2, dtdata3] = dtdata
+[x_dt_train, y_dt_train, x_dt_test, y_dt_test] = dtdata
 
-# Low rank approximation of train data
-U, s, Vh = linalg.svd(dtdata0.T)
+# Low rank approximation of diabetes train data
+U, s, Vh = linalg.svd(x_dt_train.T)
 temps = np.copy(s)
 s [2:] = 0
 sk = linalg.diagsvd(s, U.shape[1], Vh.shape[0])
 dt0_app = np.dot(U, np.dot(sk, Vh))
 dt0_app = dt0_app[:,:2]
-fig = plt.figure()
-plt.scatter(dt0_app[:,0],dt0_app[:,1])
-fig.suptitle('Diabetes data in 2-D with low rank approximation')
-fig.show()
 
+#colors = [cm.rainbow(np.linspace(0,1,np.unique(y_dt_train).size))]
+#fig = plt.figure()
+#plt.scatter(dt0_app[:,0],dt0_app[:,1])
+#fig.suptitle('Diabetes data in 2-D with low rank approximation')
+#fig.show()
 
